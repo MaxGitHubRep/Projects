74f05131e6e8fe386caed031ae68eb57cb712659
Max Home <MC170171@reigate.ac.uk>
2017-10-31 21:42:11 +0000

    Added int to string day converter.

--- src/me/max/tester/managers/time/DayOfWeek.java
+++ src/me/max/tester/managers/time/DayOfWeek.java
@@ -31,4 +31,51 @@ public class DayOfWeek {
         return simpleDateformat.format(now);
     }
     
-}
+    public int dayStrToInt(String day) {
+        
+        switch (day) {
+            case "Sunday":
+                return 1;
+            case "Monday":
+                return 2;
+            case "Tuesday":
+                return 3;
+            case "Wednesday":
+                return 4;
+            case "Thursday":
+                return 5;
+            case "Friday":
+                return 6;
+            case "Saturday":
+                return 7;
+            default:
+                return 1;
+        }
+        
+    }
+    
+    public String dayIntToStr(int day) {
+        
+        switch (day) {
+            case 1:
+                return "Sunday";
+            case 2:
+                return "Monday";
+            case 3:
+                return "Tuesday";
+            case 4:
+                return "Wednesday";
+            case 5:
+                return "Thursday";
+            case 6:
+                return "Friday";
+            case 7:
+                return "Saturday";
+            default:
+                return "null";
+            }
+        
+        }
+        
+    }
+    
